4fa088d25f94ee77f28f243c8ea805927764cc47
Integralist <mark.mcdx@gmail.com>
2020-09-16 11:11:18 +0100

    tweak Default APIs docstring and regenerate gen.go

--- hack/generate-helpers.fish
+++ hack/generate-helpers.fish
@@ -14,7 +14,7 @@ for pair in StringDefault/string IntDefault/int Int64Default/int64 Uint64Default
     set name   (echo $pair | cut -d'/' -f1)
     set gotype (echo $pair | cut -d'/' -f2)
     echo ""
-    echo "// Get$name return the value of key as type $gotype."
+    echo "// Get$name returns the value of key as type $gotype."
     echo "// If key is not set, or its value is not of type $gotype,"
     echo "// then Get$name returns defaultValue."
     echo "func (d *Data) Get$name(key string, defaultValue $gotype) $gotype {"
